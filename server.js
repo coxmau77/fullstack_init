0f7e8869fa5505154429947e32de2da26bb88bed
Mauricio Cox <coxmau77@gmail.com>
2024-07-20 04:49:00 -0300

    fix: deploy exitoso

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Actualiza la configuración de Mongoose: Para manejar el aviso de deprecación, agrega la siguiente línea al inicio de tu archivo server.js:
+mongoose.set('strictQuery', false);
+
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 import dotenv from 'dotenv';
 
 // Cargar las variables de entorno
 dotenv.config();
 
+const { DB_URI, PORT = 3000 } = process.env;
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 // Middleware para servir archivos estáticos
@@ -17,7 +22,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`*************************************************\n Servidor en ejecución en http://localhost:${PORT} \n*************************************************`);
-});
+// Conectar a la base de datos MongoDB y al servidor
+mongoose.connect(DB_URI).
+    then(() => {
+        app.listen(PORT, () => {
+            console.log(`*************************************************\n Servidor en ejecución en http://localhost:${PORT} \n*************************************************`);
+        });
+    })
+    .catch(error => console.log(error));
